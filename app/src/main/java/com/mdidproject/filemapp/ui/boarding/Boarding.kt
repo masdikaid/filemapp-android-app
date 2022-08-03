834004dd0a597b584fb1cae8e527d2e148f486d6
masdika ID <masdikaid@gmail.com>
2022-08-03 14:34:59 +0700

    hide action bar

--- app/src/main/java/com/mdidproject/filemapp/ui/boarding/Boarding.kt
+++ app/src/main/java/com/mdidproject/filemapp/ui/boarding/Boarding.kt
@@ -1,9 +1,15 @@
 package com.mdidproject.filemapp.ui.boarding
 
+import android.os.Bundle
 import com.mdidproject.filemapp.R
 import com.mdidproject.filemapp.abstraction.base.BaseActivity
 import com.mdidproject.filemapp.databinding.ActivityBoardingBinding
 
 class Boarding : BaseActivity<ActivityBoardingBinding>() {
     override fun getLayoutResID(): Int = R.layout.activity_boarding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
+        super.onCreate(savedInstanceState)
+    }
 }

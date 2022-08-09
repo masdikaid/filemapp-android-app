c1ba9a410f62b85120bd93517ef74313efda5a08
masdika ID <masdikaid@gmail.com>
2022-08-09 20:29:29 +0700

    styling calendar in signup layout

--- app/src/main/java/com/mdidproject/filemapp/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/mdidproject/filemapp/ui/auth/SignUpFragment.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Toast
+import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.mdidproject.filemapp.R
 import com.mdidproject.filemapp.abstraction.base.BaseFragment
 import com.mdidproject.filemapp.databinding.FragmentSignUpBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
     override fun getLayoutResourceId(): Int = R.layout.fragment_sign_up
@@ -31,9 +33,23 @@ class SignUpFragment : BaseFragment<FragmentSignUpBinding>() {
             binding.acGender.setAdapter(it)
         }
 
-//        binding.btnSignUp.setOnClickListener {
-//            Toast.makeText(activity as Context, binding.spnrGender.selectedItem.toString(), Toast.LENGTH_SHORT).show()
-//        }
+        binding.etBirthday.setOnClickListener { et ->
+            val datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                    .setTitleText("Select Date")
+                    .setTheme(R.style.FilemApp_Component_DatePicker)
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .build()
+            datePicker.show(parentFragmentManager, "Signup Date")
+
+            datePicker.addOnPositiveButtonClickListener {
+                val date = Date(it)
+                val format = SimpleDateFormat("dd/MM/yyyy")
+                (binding.etBirthday as TextView).text = format.format(date)
+            }
+        }
+
+
 
         super.onViewCreated(view, savedInstanceState)
     }
